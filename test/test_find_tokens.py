e2326ea1d448fd973593855ee42fa9d8f1cafa7d
Alex Southgate <southgateJA@cardiff.ac.uk>
2023-10-08 22:35:29 +0100

    some test reformatting

--- test/test_find_tokens.py
+++ test/test_find_tokens.py
@@ -1,21 +1,18 @@
 import pyunsustainable.find_tokens as ft
 
-#pytest.parameterize(...
+
+# We could use pytest.parameterize(...
 def test_edit_distance():
-    
-    # edit distance between 2 strings that are the same should be 0
-
+    # The edit distance between 2 strings that are the same should be 0
     assert ft.edit_distance("oranges", "oranges") == 0
-
     assert ft.edit_distance("sand", "land") == 1
-    
     assert ft.edit_distance("and", "land") == 1
-    
     assert ft.edit_distance("lan", "land") == 1
-
     assert ft.edit_distance("", "land") == 4
 
-    #...
+    # How else could we test this?
+    # ...
+
 
 def test_something_fails():
     """This test will fail and prevent the pre-commit hook from returning 0"""
