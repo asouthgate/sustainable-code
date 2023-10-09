ad637186d90d323811da490e4f1ed01e26d9be38
Alex Southgate <southgateJA@cardiff.ac.uk>
2023-10-09 10:01:46 +0100

    use the refactored file for testing

--- test/test_find_tokens.py
+++ test/test_find_tokens.py
@@ -1,14 +1,14 @@
-import pyunsustainable.find_tokens as ft
+import pyunsustainable.find_tokens_refactored as ft
 
 
 # We could use pytest.parameterize(...
-def test_edit_distance():
+def test_alignment_distance():
     # The edit distance between 2 strings that are the same should be 0
-    assert ft.edit_distance("oranges", "oranges") == 0
-    assert ft.edit_distance("sand", "land") == 1
-    assert ft.edit_distance("and", "land") == 1
-    assert ft.edit_distance("lan", "land") == 1
-    assert ft.edit_distance("", "land") == 4
+    assert ft.alignment_distance("oranges", "oranges") == 0
+    assert ft.alignment_distance("sand", "land") == 1
+    assert ft.alignment_distance("and", "land") == 1
+    assert ft.alignment_distance("lan", "land") == 1
+    assert ft.alignment_distance("", "land") == 4
 
     # How else could we test this?
     # ...
