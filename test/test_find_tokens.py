d812c8db829fe5c1cb8ca926306b7f18531c61fd
Alex Southgate <southgateJA@cardiff.ac.uk>
2023-10-09 10:14:01 +0100

    create a test file for the refactored case as well, create blank find_tokens test file

--- test/test_find_tokens.py
+++ test/test_find_tokens.py
@@ -1,19 +1,5 @@
-import pyunsustainable.find_tokens_refactored as ft
+import pyunsustainable.find_tokens as ft
 
 
-# We could use pytest.parameterize(...
-def test_edit_distance():
-    # The edit distance between 2 strings that are the same should be 0
-    assert ft.alignment_distance("oranges", "oranges") == 0
-    assert ft.alignment_distance("sand", "land") == 1
-    assert ft.alignment_distance("and", "land") == 1
-    assert ft.alignment_distance("lan", "land") == 1
-    assert ft.alignment_distance("", "land") == 4
-
-    # How else could we test this?
-    # ...
-
-
-def test_something_fails():
-    """This test will fail and prevent the pre-commit hook from returning 0"""
+def test_nothing():
     assert False
